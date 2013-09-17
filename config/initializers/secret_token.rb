8ec3f761b4380712d5f03b8d601d5ab77cd57ad1
Konstantin Ilchenko <simpli4eg@gmail.com>
2013-09-17 19:32:55 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Apoex::Application.config.secret_key_base = '596fdbeb78514f6fade4b67d807d89e3e6ac1bfa893c1aad43ae8ec09e1bd0a06795db48a70d393f6aa53bfeacc7212856b090b477d8ca057e109777907422df'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Apoex::Application.config.session_store :cookie_store, key: '_apoex_session'
