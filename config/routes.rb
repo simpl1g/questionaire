dadb4916ae7cf388cd4a3d6d1b63a3225c609de3
Konstantin Ilchenko <konstantin@ilchenko.by>
2013-10-21 00:31:49 +0300

    Remove database.yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Apoex::Application.routes.draw do
-  get "public/index"
+  root "public#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
