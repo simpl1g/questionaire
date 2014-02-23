ba0331651eed9319407e321b88f342c7f409e55c
Konstantin Ilchenko <konstantin@ilchenko.by>
2014-02-23 22:58:44 +0300

    Add tag model

---
+++ db/migrate/20140223195757_create_tags.rb
@@ -0,0 +1,10 @@
+class CreateTags < ActiveRecord::Migration
+  def change
+    create_table :tags do |t|
+      t.references :taggable, index: true
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
