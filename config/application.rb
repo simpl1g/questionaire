aaf98b5d57b061f15c68a8b99931072bc561e91a
Konstantin Ilchenko <konstantin@ilchenko.by>
2014-03-02 21:20:40 +0300

    fix fonts

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,6 @@ module Apoex
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   end
 end
