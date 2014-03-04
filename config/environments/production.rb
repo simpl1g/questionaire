0de1feaddfd2cfeaca4128cd81a3fa0851fd53b1
Konstantin Ilchenko <konstantin@ilchenko.by>
2014-03-05 00:07:45 +0300

    revert js compression

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ LinkBase::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :yui
+  config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :yui
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
