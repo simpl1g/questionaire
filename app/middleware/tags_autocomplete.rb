7779c7daf033b26c752ea6355b681f9a99930e5d
Konstantin Ilchenko <konstantin@ilchenko.by>
2014-03-16 22:29:14 +0300

    reset db connections in middleware

--- app/middleware/tags_autocomplete.rb
+++ app/middleware/tags_autocomplete.rb
@@ -8,6 +8,7 @@ class TagsAutocomplete
       request = Rack::Request.new(env)
       term = request.params['term']
       tags = ActsAsTaggableOn::Tag.where("tags.name LIKE ?", "#{term}_%").limit(10).pluck(:name)
+      ActiveRecord::Base.clear_active_connections!
       [200, {'Content-Type' => 'application/json'}, [tags.to_json]]
     else
       @app.call(env)
